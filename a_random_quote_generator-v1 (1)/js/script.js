e466065091a02f0bbb9c7570706137997dc17cde
zsreed93 <53982883+zsreed93@users.noreply.github.com>
2020-06-29 11:32:24 -0400

    added quotes

--- a_random_quote_generator-v1 (1)/js/script.js
+++ a_random_quote_generator-v1 (1)/js/script.js
@@ -10,7 +10,33 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
+  {
+    quote: "I dedicated all the time I had to it. The 10 hour workout was just what I put in the magazine at the time, but for me it was every waking moment.",
+    source: "Steve Vai"
+  },
+  {
+    quote: "A lot of people think that if they learn to read music they are gonna lose their feel or their groove or something. It\’s the stupidest thing I have ever heard.",
+    source: "Frank Gambale"
 
+  },
+  {
+    quote: "I started out playing guitar because Jimi Hendrix was my hero, so my roots were really based on Jimi Hendrix and his style of playing.",
+    source: "Joe Satriani"
+
+  },
+  {
+    quote:"The beautiful thing about learning is nobody can take it away from you.",
+    source: "B.B. King"
+
+  },
+  {
+    quote:"When I look for what I’m going to listen to I go backwards. I’m always going the other way you see. Most people are trying to figure out how do I get in the fast lane going that way?. I\'m going in the other direction. I wanna find the oldest thing to do.",
+    source: "Eric Clapton"
+
+  }
+
+];
 
 
 /***
